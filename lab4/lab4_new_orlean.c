895b645764a900fbde61515e8df4d1df6a6f0e05
VKostovsky <SpasiboMiryZaZemfiry@gmail.com>
2025-04-11 22:40:57 +0700

    lab4: used {fcntl; dup}

--- lab4/lab4_new_orlean.c
+++ lab4/lab4_new_orlean.c
@@ -30,13 +30,24 @@ int main() {
     pid_t ls_pid = fork();
     if (ls_pid == 0) {
         printf("Создан процесс для ls -a (PID: %d)\n", getpid());
+
+        // Открываем файл a.txt для записи
         int fd = open("a.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (fd == -1) {
             perror("Ошибка открытия a.txt");
             exit(EXIT_FAILURE);
         }
-        dup2(fd, STDOUT_FILENO);
+
+        // Закрываем стандартный вывод (дескриптор 1)
+        close(STDOUT_FILENO);
+
+        // Создаем копию дескриптора fd и присваиваем его дескриптору 1
+        fcntl(fd, F_DUPFD, STDOUT_FILENO);
+
+        // Закрываем оригинальный дескриптор fd
         close(fd);
+
+        // Выполняем команду ls -a
         execlp("ls", "ls", "-a", NULL);
         perror("Ошибка execlp ls");
         exit(EXIT_FAILURE);
@@ -51,13 +62,24 @@ int main() {
     pid_t who_pid = fork();
     if (who_pid == 0) {
         printf("Создан процесс для who (PID: %d)\n", getpid());
+
+        // Открываем файл b.txt для записи
         int fd = open("b.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (fd == -1) {
             perror("Ошибка открытия b.txt");
             exit(EXIT_FAILURE);
         }
-        dup2(fd, STDOUT_FILENO);
+
+        // Закрываем стандартный вывод (дескриптор 1)
+        close(STDOUT_FILENO);
+
+        // Создаем копию дескриптора fd и присваиваем его дескриптору 1
+        fcntl(fd, F_DUPFD, STDOUT_FILENO);
+
+        // Закрываем оригинальный дескриптор fd
         close(fd);
+
+        // Выполняем команду who
         execlp("who", "who", NULL);
         perror("Ошибка execlp who");
         exit(EXIT_FAILURE);
@@ -79,9 +101,20 @@ int main() {
     pid_t cat_pid = fork();
     if (cat_pid == 0) {
         printf("Создан процесс для cat (PID: %d)\n", getpid());
+
+        // Закрываем конец канала для чтения
         close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
+
+        // Закрываем стандартный вывод (дескриптор 1)
+        close(STDOUT_FILENO);
+
+        // Создаем копию дескриптора pipefd[1] и присваиваем его дескриптору 1
+        fcntl(pipefd[1], F_DUPFD, STDOUT_FILENO);
+
+        // Закрываем оригинальный дескриптор pipefd[1]
         close(pipefd[1]);
+
+        // Выполняем команду cat
         execlp("cat", "cat", "a.txt", "b.txt", NULL);
         perror("Ошибка execlp cat");
         exit(EXIT_FAILURE);
@@ -93,9 +126,20 @@ int main() {
     pid_t sort_pid = fork();
     if (sort_pid == 0) {
         printf("Создан процесс для sort (PID: %d)\n", getpid());
+
+        // Закрываем конец канала для записи
         close(pipefd[1]);
-        dup2(pipefd[0], STDIN_FILENO);
+
+        // Закрываем стандартный ввод (дескриптор 0)
+        close(STDIN_FILENO);
+
+        // Создаем копию дескриптора pipefd[0] и присваиваем его дескриптору 0
+        fcntl(pipefd[0], F_DUPFD, STDIN_FILENO);
+
+        // Закрываем оригинальный дескриптор pipefd[0]
         close(pipefd[0]);
+
+        // Выполняем команду sort
         execlp("sort", "sort", NULL);
         perror("Ошибка execlp sort");
         exit(EXIT_FAILURE);
@@ -104,9 +148,11 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Закрываем оба конца канала в родительском процессе
     close(pipefd[0]);
     close(pipefd[1]);
 
+    // Ожидаем завершения процессов
     waitpid(cat_pid, NULL, 0);
     printf("Процесс cat завершен (PID: %d)\n", cat_pid);
     waitpid(sort_pid, NULL, 0);
